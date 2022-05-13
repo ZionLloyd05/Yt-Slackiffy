f6e7ae01290dc58eb2489fce2043d1b16f862619
Alagbala Damilola <alagbaladamilola@gmail.com>
2022-05-13 13:25:42 +0100

    Add project files.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+﻿module.exports = {
+    mode: 'jit',
+    content: ['./**/*.razor', './wwwroot/index.html'],
+    theme: {
+        extend: {
+            colors: {
+                slack_bg: "#3F0F40"
+            }
+        },
+    },
+    plugins: [],
+};
