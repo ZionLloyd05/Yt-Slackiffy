3a835b1bfb243c7947d21e948c6cc3b29d6a12bd
Alagbala Damilola <alagbaladamilola@gmail.com>
2022-06-07 04:37:01 +0100

    completed auth

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slackiffy.APIs
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Home() =>
+            RedirectToPage("Index");
+
+        [HttpGet("google-login")]
+        public async Task LoginAsync()
+        {
+            await HttpContext.ChallengeAsync
+                (GoogleDefaults.AuthenticationScheme, new AuthenticationProperties()
+            {
+             RedirectUri = "/"
+            });
+        }
+
+        [HttpGet("signout")]
+        public async Task<IActionResult> LogOutAsync()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("~/");
+
+        }
+    }
+}
